0e9b3abba6e6474649036491649be72ada9f822d
Christian Wesselhoeft <christian@cwesselhoeft.com>
2015-02-22 17:45:02 -0700

    Move fixtures to their own module.

--- homework2/tests/test.hs
+++ homework2/tests/test.hs
@@ -1,5 +1,6 @@
 import Test.Tasty
 import Test.Tasty.HUnit
+import Fixtures as F
 
 import Log
 import LogAnalysis
@@ -9,31 +10,6 @@ main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "Tests" [unitTests]
-
-sampleLog :: String
-sampleLog = unlines
-  [ "I 6 Completed armadillo processing"
-  , "E 70 3 Way too many pickles"
-  , "I 4 Everything normal"
-  ]
-
-sampleLog2 :: String
-sampleLog2 = unlines
-  [ "I 6 Completed armadillo processing"
-  , "I 1 Nothing to report"
-  , "E 99 10 Flange failed!"
-  , "I 4 Everything normal"
-  , "I 11 Initiating self-destruct sequence"
-  , "E 70 3 Way too many pickles"
-  , "E 65 8 Bad pickle-flange interaction detected"
-  , "W 5 Flange is due for a check-up"
-  , "I 7 Out for lunch, back in two time steps"
-  , "E 20 2 Too many pickles"
-  , "I 9 Back from lunch"
-  ]
-
-messageAt :: Int -> LogMessage
-messageAt t = LogMessage Info t (show t)
 
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
@@ -50,7 +26,7 @@ unitTests = testGroup "Unit tests"
       (parseMessage "Not the right format") @?= Unknown "Not the right format"
 
   , testCase "parse" $
-      (parse sampleLog) @?=
+      (parse F.sampleLog) @?=
         [ LogMessage Info 6 "Completed armadillo processing"
         , LogMessage (Error 70) 3 "Way too many pickles"
         , LogMessage Info 4 "Everything normal"
@@ -60,21 +36,21 @@ unitTests = testGroup "Unit tests"
       (insert (Unknown "foo") Leaf) @?= Leaf
 
   , testCase "insert (lt)" $
-      (insert (messageAt 1) (Node Leaf (messageAt 2) Leaf)) @?=
+      (insert (F.messageAt 1) (Node Leaf (F.messageAt 2) Leaf)) @?=
         Node
-          (Node Leaf (messageAt 1) Leaf)
-          (messageAt 2)
+          (Node Leaf (F.messageAt 1) Leaf)
+          (F.messageAt 2)
           Leaf
 
   , testCase "insert (gt)" $
-      (insert (messageAt 2) (Node Leaf (messageAt 1) Leaf)) @?=
+      (insert (F.messageAt 2) (Node Leaf (F.messageAt 1) Leaf)) @?=
         Node
           Leaf
-          (messageAt 1)
-          (Node Leaf (messageAt 2) Leaf)
+          (F.messageAt 1)
+          (Node Leaf (F.messageAt 2) Leaf)
 
   , testCase "build" $
-      (build $ parse sampleLog) @?=
+      (build $ parse F.sampleLog) @?=
         Node
           (Node
             Leaf
@@ -88,14 +64,14 @@ unitTests = testGroup "Unit tests"
           Leaf
 
   , testCase "inOrder" $
-      (inOrder $ build $ parse sampleLog) @?=
+      (inOrder $ build $ parse F.sampleLog) @?=
         [ LogMessage (Error 70) 3 "Way too many pickles"
         , LogMessage Info 4 "Everything normal"
         , LogMessage Info 6 "Completed armadillo processing"
         ]
 
   , testCase "whatWentWrong" $
-    (whatWentWrong $ parse sampleLog2) @?=
+    (whatWentWrong $ parse F.sampleLog2) @?=
       [ "Way too many pickles"
       , "Bad pickle-flange interaction detected"
       , "Flange failed!"
