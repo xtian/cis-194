ebb732d33be7a75b355f6c9df0d8592fd3714b35
Christian Wesselhoeft <christian@cwesselhoeft.com>
2015-03-10 12:16:02 -0700

    Homework 5, Exercise 2

--- homework5/src/Calc.hs
+++ homework5/src/Calc.hs
@@ -7,3 +7,9 @@ eval :: ExprT -> Integer
 eval (Lit x) = x
 eval (Add x y) = eval x + eval y
 eval (Mul x y) = eval x * eval y
+
+evalStr :: String -> Maybe Integer
+evalStr = maybeEval . parseExp Lit Add Mul
+  where
+    maybeEval (Just x) = Just $ eval x
+    maybeEval _ = Nothing
--- homework5/tests/test.hs
+++ homework5/tests/test.hs
@@ -11,4 +11,13 @@ unitTests :: TestTree
 unitTests = testGroup "Unit tests"
   [ testCase "eval" $
       (eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4))) @?= 20
+
+  , testCase "evalStr" $
+      (evalStr "(2+3)*4") @?= Just 20
+
+  , testCase "evalStr (no parens)" $
+      (evalStr "2+3*4") @?= Just 14
+
+  , testCase "evalStr (invalid)" $
+      (evalStr "2+3*") @?= Nothing
   ]
