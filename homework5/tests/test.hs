a2849bbe28e974b978c3e6ceae9997b2b09f9661
Christian Wesselhoeft <christian@cwesselhoeft.com>
2015-03-10 08:52:28 -0700

    Homework 5, Exercise 1

--- homework5/tests/test.hs
+++ homework5/tests/test.hs
@@ -2,10 +2,13 @@ import Test.Tasty
 import Test.Tasty.HUnit
 
 import Calc
+import ExprT
 
 main :: IO ()
 main = defaultMain unitTests
 
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
-  []
+  [ testCase "eval" $
+      (eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4))) @?= 20
+  ]
