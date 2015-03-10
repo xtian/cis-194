007e0966022e85f5aa5957a400de13922125644a
Christian Wesselhoeft <christian@cwesselhoeft.com>
2015-03-10 13:48:16 -0700

    Homework 5, Exercise 4

--- homework5/src/Calc.hs
+++ homework5/src/Calc.hs
@@ -22,3 +22,31 @@ instance Expr ExprT where
   lit = Lit
   add = Add
   mul = Mul
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit = (> 0)
+  add = (||)
+  mul = (&&)
+
+
+newtype MinMax = MinMax Integer
+  deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax x) (MinMax y) = MinMax $ max x y
+  mul (MinMax x) (MinMax y) = MinMax $ min x y
+
+
+newtype Mod7 = Mod7 Integer
+  deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit = Mod7 . max 0 . min 6
+  add (Mod7 x) (Mod7 y) = Mod7 $ (x + y) `mod` 7
+  mul (Mod7 x) (Mod7 y) = Mod7 $ (x * y) `mod` 7
--- homework5/tests/test.hs
+++ homework5/tests/test.hs
@@ -3,9 +3,13 @@ import Test.Tasty.HUnit
 
 import Calc
 import ExprT
+import Parser
 
 main :: IO ()
 main = defaultMain unitTests
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
 
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
@@ -21,7 +25,19 @@ unitTests = testGroup "Unit tests"
   , testCase "evalStr (invalid)" $
       (evalStr "2+3*") @?= Nothing
 
-  , testCase "Expr typeclass" $
+  , testCase "Expr ExprT" $
       (mul (add (lit 2) (lit 3)) (lit 4) :: ExprT) @?=
         Mul (Add (Lit 2) (Lit 3)) (Lit 4)
+
+  , testCase "Expr Integer" $
+      (testExp :: Maybe Integer) @?= Just (-7)
+
+  , testCase "Expr Bool" $
+      (testExp :: Maybe Bool) @?= Just True
+
+  , testCase "Expr MinMax" $
+      (testExp :: Maybe MinMax) @?= Just (MinMax 5)
+
+  , testCase "Expr Mod7" $
+      (testExp :: Maybe Mod7) @?= Just (Mod7 5)
   ]
