4a85ea4772e233a826e0766db24057af8918a1f4
Christian Wesselhoeft <christian@cwesselhoeft.com>
2015-03-17 12:04:26 -0700

    Homework 6, Exercise 3

--- homework6/src/Fibonacci.hs
+++ homework6/src/Fibonacci.hs
@@ -11,3 +11,11 @@ fibs1 = map fib [0..]
 fibs2 :: [Integer]
 fibs2 = map fst $ iterate fn (0, 1)
   where fn (x, y) = (y, x + y)
+
+data Stream a = Element a (Stream a)
+
+instance Show a => Show (Stream a) where
+  show = show . take 20 . streamToList
+
+streamToList :: Stream a -> [a]
+streamToList (Element x s) = x : streamToList s
